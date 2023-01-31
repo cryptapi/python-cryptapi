6dedf2155992ad13f40f70252f3c1b6fbe0cb116
Ariano Ângelo <52960493+arianoangelo@users.noreply.github.com>
2023-01-31 10:02:49 +0000

    v1.0.1 - Minor fixes

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+
+    name='python-cryptapi',
+
+    version='1.0.1',
+
+    packages=find_packages(),
+
+    author="CryptAPI",
+
+    author_email="info@cryptapi.io",
+    install_requires=[
+        'django',
+        'requests',
+    ],
+    description="Python Library for CryptAPI payment gateway",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+
+    include_package_data=True,
+
+    url='https://github.com/cryptapi/python-cryptapi',
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 5 - stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+
+    license="MIT",
+
+    zip_safe=False,
+)
